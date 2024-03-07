b26c47bb69879ee1827b5604370b1e4cf973ecac
Ubuntu <ubuntu@ip-172-31-19-238.eu-west-1.compute.internal>
2024-03-07 09:20:29 +0000

    wlst

---
+++ notas/dia4.md
@@ -0,0 +1,19 @@
+# Tamaño del pool (máximo)
+# Leer los datos de un fichero proerties
+# Test query al fichero de parametros
+# Y que pueda haber muchos ficheros
+# Que el programa reciba un dato: carpeta que contiene los ficheros properties de datasources
+# datasource1.properties , datasource2.properties, datasource3.properties
+# IDEMPOTENCIA: Que de igual el estado de partida, que el script funcione...
+# y me deje el sistema en el mismo estado siempre.
+
+# Varios servidores
+
+
+# DEVOPS
+
+Es la cultura de la automatización.
+Un movimiento global en pro de la automatización.
+De qué? De todo lo que hay entre el DEV de un sistema y su OPS .
+
+Las AUTOMATIZACIONES
--- scripts/connect.py
+++ scripts/connect.py
@@ -1,19 +1,44 @@
+# docker exec -it -w /scripts miwl wlst.sh connect.py -loadProperties datos-conexion.properties -d datasources
 import sys
 import os
+import weblogic
+
+def imprimir_mensaje_importante(mensaje):
+    line = "=" * len(mensaje)
+    print("\n" + line)
+    print(mensaje)
+    print(line + "\n")
+    
+def imprimir_info_datasource(nombre, jndi, url, driver, usuario, servidor):
+    print("-" * 20)
+    print(" DataSource: %s" % nombre)
+    print(" JNDI: %s" % jndi)
+    print(" URL: %s" % url)
+    print(" Driver: %s" % driver)
+    print(" Usuario: %s" % usuario)
+    print(" Servidor: %s" % servidor)
+
+    print("-" * 20 + "\n")
 
 def crear_propiedad(ruta, propiedad):
     cd(ruta)
-    cmo.createProperty(propiedad)
-    
+    try:
+        cmo.createProperty(propiedad)
+    except weblogic.descriptor.BeanAlreadyExistsException:
+        print("La propiedad "+propiedad+" ya existe para el recurso "+ruta+". Procedemos a su edicion.")
+
 
 def asignar_propiedad(ruta, propiedad, valor):
     cd(ruta)
     set(propiedad,valor)
 
 
-def crear_datasource(nombre, jndi, url, driver, usuario, password, servidor):
+def crear_datasource(nombre, jndi, url, driver, usuario, password, servidor, test_query, conexionesMaximas):
     cd('/')
-    cmo.createJDBCSystemResource(nombre)
+    try:
+        cmo.createJDBCSystemResource(nombre)
+    except weblogic.descriptor.BeanAlreadyExistsException:
+        print("El datasource ya existe. Procedemos a su edicion.")
     
     ruta_recurso = '/JDBCSystemResources/'+nombre+'/JDBCResource/'+nombre
     
@@ -29,11 +54,48 @@ def crear_datasource(nombre, jndi, url, driver, usuario, password, servidor):
     crear_propiedad(  ruta_recurso+'/JDBCDriverParams/'+nombre+'/Properties/'+nombre, 'user')
     asignar_propiedad(ruta_recurso+'/JDBCDriverParams/'+nombre+'/Properties/'+nombre+'/Properties/user', 'Value', usuario)
     
-    asignar_propiedad(ruta_recurso+'/JDBCConnectionPoolParams/'+nombre, 'TestTableName', 'SQL SELECT 1')
+    asignar_propiedad(ruta_recurso+'/JDBCConnectionPoolParams/'+nombre, 'TestTableName', test_query)
+    asignar_propiedad(ruta_recurso+'/JDBCConnectionPoolParams/'+nombre, 'InitialCapacity', 1)
+    asignar_propiedad(ruta_recurso+'/JDBCConnectionPoolParams/'+nombre, 'MaxCapacity', conexionesMaximas)
     
     asignar_propiedad('/JDBCSystemResources/'+nombre, 'Targets',jarray.array([ObjectName('com.bea:Name='+servidor+',Type=Server')], ObjectName))
 
-
+def crear_datasources():
+    imprimir_mensaje_importante("Creando datasources.")
+    carpeta_datasources=None
+    for indice in range(1, len(sys.argv)): 
+        parametro = sys.argv[indice]
+        if parametro == "--datasources" or parametro == "-d":
+            carpeta_datasources = sys.argv[indice + 1]
+    if carpeta_datasources is not None:
+        for datasource_index in range(1, 1000):
+            try:
+                fichero = carpeta_datasources+"/datasource"+str(datasource_index)+".properties"
+                if not os.path.exists(fichero):
+                    break
+                loadProperties(fichero)
+                print("Procesando fichero "+fichero)
+                imprimir_info_datasource(datasource_nombre,
+                                datasource_jndi,
+                                datasource_url,
+                                datasource_driver,
+                                datasource_usuario,
+                                datasource_servidor)
+                crear_datasource(datasource_nombre,
+                                datasource_jndi,
+                                datasource_url,
+                                datasource_driver,
+                                datasource_usuario,
+                                datasource_password,
+                                datasource_servidor,
+                                datasource_consultaDePrueba,
+                                datasource_conexionesMaximas)
+            except NameError,e:
+                print("No se ha definido la propiedad del datasource "+str(e))
+            except:
+                break
+    else:
+        print("No se han encontrado datasources que cargar.")
 def pedir_dato_por_consola(mensaje, valor_por_defecto):
     dato = raw_input(mensaje + " [" + valor_por_defecto + "]: ")  
     if len(dato) == 0:
@@ -89,6 +151,28 @@ def inicializar_dato(valor_fichero, valor_codigo):
 
 def super_connect(_usuario=None, _password=None, _protocolo=None, _host=None, _puerto=None):
     global usuario, password, protocolo, host, puerto
+    try: 
+        usuario
+    except NameError:
+        usuario = None
+    try:
+        password
+    except NameError:
+        password = None
+    try:
+        protocolo
+    except NameError:
+        protocolo = None
+    try:
+        host
+    except NameError:
+        host = None
+    try:
+        puerto
+    except NameError:
+        puerto = None
+        
+
     usuario = inicializar_dato(usuario, _usuario)
     password = inicializar_dato(password, _password)
     protocolo = inicializar_dato(protocolo, _protocolo)
@@ -104,7 +188,7 @@ def super_connect(_usuario=None, _password=None, _protocolo=None, _host=None, _p
     print(" Password: %s" % password)
     try:
         connect(usuario, password, url) # Llamo al weblogic
-        print("Conectados al servidor")
+        imprimir_mensaje_importante("Conectado al servidor weblogic.")
     except:
         print("Error: No ha sido posible conectarse con el servidor.")
         exit(1)
@@ -113,21 +197,24 @@ super_connect(_usuario="Weblogic", _protocolo="t3s", _host="localhost", _puerto=
 
 edit()
 startEdit()
+imprimir_mensaje_importante("Editando el dominio.")
 try:
-    crear_datasource("Mi Mysql", 
-                     "jdbc/mysql", 
-                     "jdbc:mysql://mysql:3306/basedatos", 
-                     "com.mysql.jdbc.Driver", 
-                     "usuario", 
-                     "password", 
-                     "AdminServer")
-        
+    #crear_datasource("Mi Mysql", 
+    #                 "jdbc/mysql", 
+    #                 "jdbc:mysql://mysql:3306/basedatos", 
+    #                 "com.mysql.jdbc.Driver", 
+    #                 "usuario", 
+    #                 "password", 
+    #                 "AdminServer")
+    crear_datasources()
+    imprimir_mensaje_importante("Guardando y aplicando los cambios.")
     save()
     activate()
 except:
-    print("Ocurrio un error:")
+    imprimir_mensaje_importante("Ha ocurrido un error al realizar los cambios.")
     import traceback
     apply(traceback.print_exception, sys.exc_info())
     stopEdit('y')
-    
+
+imprimir_mensaje_importante("Desconectando del servidor weblogic.")
 disconnect()
